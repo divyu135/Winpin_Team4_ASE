f27b8dac057a4f83d6edd3f8d4514c25151003e2
Divyajeet Chauhan <divyajeetchauhan@gmail.com>
2020-11-01 01:36:11 +0530

    Add HomePage made using Sematic-UI

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import HomePage from './Layout/HomePage'
+import '../style/App.css';
+import 'semantic-ui-css/semantic.min.css'
+
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
