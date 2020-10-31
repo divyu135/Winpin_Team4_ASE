75bb622cbbca11603d29b00c5ebc735d1a8ac60c
Vyoma Kanani <vyomapatel01@gmail.com>
2020-10-31 21:42:17 +0530

    Initial stage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import MainLayout from './Components/Layout/mainLayout';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainLayout />
   </React.StrictMode>,
   document.getElementById('root')
 );
