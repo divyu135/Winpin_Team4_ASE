80a3c573ab846ae1dc78e59d1a8b9248aec54120
Vyoma Kanani <vyomapatel01@gmail.com>
2020-11-04 13:32:46 +0530

    main layout

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,24 @@
 import HomePage from './Layout/HomePage'
+import Video from './Video/video'
+import Navigation from './Layout/navigation'
 import '../style/App.css';
 import 'semantic-ui-css/semantic.min.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+}from 'react-router-dom'
 
 function App() {
   return (
-    <HomePage />
+    <Router>
+       <Navigation/>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/home"  component={HomePage} />
+        <Route path="/video"  component={Video} />
+      </Switch>
+    </Router>
   );
 }
 
