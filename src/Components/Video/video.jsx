05da05295eb16480e0968385ca7b720fad622139
Vyoma Kanani <vyomapatel01@gmail.com>
2020-11-04 13:39:27 +0530

    main layout

---
+++ src/Components/Video/video.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Segment } from "semantic-ui-react";
+
+class video extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Segment style={{ padding: "8em 0em" }} vertical>
+          <h1>Video page</h1>
+          <h1>Video page</h1>
+          <h1>Video page</h1>
+          <h1>Video page</h1>
+          <h1>Video page</h1>
+        </Segment>
+      </div>
+    );
+  }
+}
+
+export default video;
