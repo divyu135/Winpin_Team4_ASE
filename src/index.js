a8f5d58cdb3c114487ae4e123da34b41556e9bc3
Vyoma Kanani <vyomapatel01@gmail.com>
2020-11-09 21:51:15 +0530

    changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style/index.css';
-import App from './Components/App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-=======
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-// import 'bootstrap/dist/css/bootstrap.css';
-
+import "./style/index.css";
 import App from "./Components/App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(<App />, document.getElementById("root"));
->>>>>>> MainLayout
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
