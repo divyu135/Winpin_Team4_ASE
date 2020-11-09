7272b328139ed20210ca12fe8156cde55463a640
Vyoma Kanani <vyomapatel01@gmail.com>
2020-11-09 21:29:51 +0530

    merging MainLayout

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import HomePage from './Layout/HomePage'
-import Video from './Video/video'
-import Navigation from './Layout/navigation'
-import '../style/App.css';
-import 'semantic-ui-css/semantic.min.css'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-}from 'react-router-dom'
-=======
 /* eslint-disable max-classes-per-file */
 /* eslint-disable react/no-multi-comp */
 
@@ -20,24 +8,15 @@ import Navigation from "./Layout/Navigation";
 import "semantic-ui-css/semantic.min.css";
 // import '@semantic-ui-react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
->>>>>>> MainLayout
 
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
-       <Navigation/>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/home"  component={HomePage} />
-        <Route path="/video"  component={Video} />
-=======
       <Navigation />
       <Switch>
         <Route path='/' exact component={HomePage} />
         <Route path='/home' component={HomePage} />
         <Route path='/video' component={Video} />
->>>>>>> MainLayout
       </Switch>
     </Router>
   );
