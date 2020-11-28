260456788705e75e9d374fc5e01d76ce43c6b30c
Hussain-MD <hussai23@uwindsor.ca>
2020-11-28 11:42:30 +0530

    setup for ibm db2 database

--- winpin-server/server/server.js
+++ winpin-server/server/server.js
@@ -1,6 +1,6 @@
 // // import dependencies and initialize express
 // const express = require('express');
-const path = require('path');
+// const path = require('path');
 // const bodyParser = require('body-parser');
 // const cors = require('cors');
 
@@ -16,7 +16,7 @@ const path = require('path');
 // const app = express();
 
 // app.use(cors);
-
+// 
 // // enable parsing of http request body
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
@@ -49,11 +49,48 @@ const path = require('path');
 
 // module.exports = app;
 
-const express = require('express')
+// const express = require('express')
+// var routes = require('./database/routes');
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var ibmdb = require('ibm_db');
+
 const app = express()
 
+// connecting to IBM DB2 database
+
+//setting up database routes
+
+const listSysTables = require('./database/routes/hello-route');
+
+
+app.set('views', path.join(__dirname, 'database','views'));
+app.set('view engine', 'jade');
+
+var db2;
+var hasConnect = false;
+
+if ( hasConnect == false ) {
+
+   db2 = {
+        db: "BLUDB",
+        hostname: "dashdb-txn-sbox-yp-lon02-06.services.eu-gb.bluemix.net",
+        port: 50000,
+        username: "ssg47540",
+        password: "c4plzs7h2kpjt0@2"
+     };
+}
+
+var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
+
+//making connection
+app.get('/db2', listSysTables(ibmdb,connString));
+
+
 // serve the react app files
-console.log(path.join(__dirname, '../winpin'));
+// console.log(path.join(__dirname, '../winpin'));
 app.use(express.static(path.join(__dirname, '../winpin-ui')));
 app.get('/api/hello', function(req,res) {
 	res.json({message:"Hello World"});
